c716c69d5e8b54651706c25f72dd66281ce46eb2
kwamboka1 <makorimildred@gmail.com>
2022-07-15 13:15:28 +0300

    [Update]: The css of the the App

--- 0x01-react_intro/task_2/dashboard/src/utils.js
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -1,3 +1,6 @@
+/* full-year */
 export const getFullYear = () => new Date().getFullYear();
 
+/* footer-copy */
 export const getFooterCopy = (isIndex) => (isIndex ? "Holberton School" : "Holberton School main dashboard");
+
