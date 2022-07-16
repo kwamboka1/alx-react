a1800b408cc427f2fccaaa0fe2e33cd9a5aa5cb9
kwamboka1 <makorimildred@gmail.com>
2022-07-17 00:37:10 +0300

    [ Create]: A project using Webpack

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,40 +1,54 @@
-entry: {
-  main: path.resolve(__dirname, './src/app.js'),
-},
-output: {
-  filename: '[name].bundle.js',
-  path: path.resolve(__dirname, 'deploy')
-},
-module: {
-  rules: [
-    {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env']
-        }
-      }
-    },
-  ]
-},
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        // type: 'asset/resource',
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  devServer: {
+    static: "./dist",
+    compress: true,
+    open: true,
+    hot: true,
+    port: 8564,
+  },
+  devtool: "inline-source-map",
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Webpack Output",
+      name: "index.html",
+      inject: false,
+      template: "./dist/index.html",
     }),
   ],
 };
-module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      }
-    ]
-  }
-
