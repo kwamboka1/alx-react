b5cfacebe6d623521f0e4cd5f2d2c3659a1285f6
kwamboka1 <makorimildred@gmail.com>
2022-07-18 11:27:02 +0300

    [Reorganize]: The files

---
+++ 0x01-react_intro/task_5/dashboard/src/Notifications/Notifications.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Notifications from "./Notifications";
+
+const notification = shallow(<Notifications />);
+
+describe("Notification component tests", () => {
+  it("renders Notification component without crashing", () => {
+    expect(notification).toBeDefined();
+  });
+
+  it("renders ul", () => {
+    expect(notification.find("ul")).toBeDefined();
+  });
+
+  it("renders three list items", () => {
+    expect(notification.find("li")).toHaveLength(3);
+  });
+
+  it("renders correct text", () => {
+    expect(notification.find("p").text()).toBe("Here is the list of notifications");
+  });
+});
