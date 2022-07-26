b8c54b9c16c1a32e2d3445aeab685ed3b1559ed3
kwamboka1 <makorimildred@gmail.com>
2022-07-26 14:37:03 +0300

    [Commence]: With class components

---
+++ 0x03-React_component/task_0/dashboard/src/Footer/Footer.test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "./Footer";
+import { getFullYear, getFooterCopy } from "../utils/utils";
+import { shallow } from "enzyme";
+
+describe("Footer test", () => {
+  it("should render without crashing", () => {
+    const wrapper = shallow(<Footer />);
+    expect(wrapper.exists()).toEqual(true);
+  });
+  it("should render the text Copyright", () => {
+    const wrapper = shallow(<Footer />);
+    expect(wrapper.text()).toEqual(`Copyright ${getFullYear()} - ${getFooterCopy()}`);
+  });
+});
