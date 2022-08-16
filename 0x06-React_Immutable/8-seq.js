390a280be46bc774588d86daa1aefa7268826738
kwamboka1 <makorimildred@gmail.com>
2022-08-16 16:06:30 +0300

    [Lazy Seq

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -4,21 +4,20 @@ export default function printBestStudents(object) {
   const seq = Seq(object);
 
   const chosen = seq.filter((student) => {
-    student.firstName.charAt(0).toUpperCase();
-      return student.score > 70;
-    });
+  student.firstName.charAt(0).toUpperCase();
+  return student.score > 70;
+  });
 
-    function capFirstLetter(string) {
-      return string.charAt(0).toUpperCase() + string.slice(1);
-    }
+  function capFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+  }
 
-    const OurObject = chosen.toJS();
+  const OurObject = chosen.toJS();
 
-    Object.keys(OurObject).map((key) => {
-      OurObject[key].firstName = capFirstLetter(OurObject[key].firstName);
-      OurObject[key].lastName = capFirstLetter(OurObject[key].lastName);
-      return OurObject[key];
-    });
-
-    console.log(OurObject);
-}
+  Object.keys(OurObject).map((key) => {
+  OurObject[key].firstName = capFirstLetter(OurObject[key].firstName);
+  OurObject[key].lastName = capFirstLetter(OurObject[key].lastName);
+  return OurObject[key];
+  });
+  console.log(OurObject);
+  }
