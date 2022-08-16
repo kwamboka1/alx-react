b948390a75600eb689a28e997b59a322e6297a71
kwamboka1 <makorimildred@gmail.com>
2022-08-16 15:33:16 +0300

    [Merge & concat

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,16 +1,13 @@
-import { List } from './node_modules/immutable/dist/immutable';
-import { Map } from './node_modules/immutable/dist/immutable';
+import { List, Map } from 'immutable';
 
-export default function concatElements(page1, page2) {
+export function concatElements(page1, page2) {
   const list1 = List(page1);
   const list2 = List(page2);
-
   return list1.concat(list2);
 }
 
-export default function mergeElements(page1, page2) {
-  const map1 = Map(page1);
-  const map2 = Map(page2);
-
-  return map1.merge(map2);
+export function mergeElements(page1, page2) {
+  const object1 = Map(page1);
+  const object2 = Map(page2);
+  return object1.merge(object2);
 }
